517cf6c79622c67255896fa19546f6617ca3ea28
Michal_T <michal.trostowiecki@gmail.com>
2024-02-27 20:29:53 +0000

    Add Flowbite React, Tailwind CSS

--- foodie-cal-quest/src/App.jsx
+++ foodie-cal-quest/src/App.jsx
@@ -1,16 +1,14 @@
-
-
-
 import './App.css'
+import { Button } from 'flowbite-react';
 
 function App() {
   
 
-  return (
-    <div>
-      <h1>Foodie Cal Quest</h1>
-    </div>
-  )
+	return (
+			<div>
+				<h1>Foodie Cal Quest</h1>
+			</div>
+		)
 }
 
 export default App
