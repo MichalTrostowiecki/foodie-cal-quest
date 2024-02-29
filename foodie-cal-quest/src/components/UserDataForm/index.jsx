39b7ef62d81367c9ca2a4a859d7b0ed2a344e9fd
Michal_T <michal.trostowiecki@gmail.com>
2024-02-29 21:38:42 +0000

    Test commit

--- foodie-cal-quest/src/components/UserDataForm/index.jsx
+++ foodie-cal-quest/src/components/UserDataForm/index.jsx
@@ -110,6 +110,7 @@ const UserDataForm = ({ setUser }) => {
                 </select>
             </div>
             {/* End of Activity Level Section  */}
+            {/* Test comment  */}
             
             <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Calculate</button>
         </form>
