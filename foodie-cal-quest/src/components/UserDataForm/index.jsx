585998903de7a6665d3da302b6a8d88bf5b0e133
Michal_T <michal.trostowiecki@gmail.com>
2024-02-29 21:33:58 +0000

    Test commit

--- foodie-cal-quest/src/components/UserDataForm/index.jsx
+++ foodie-cal-quest/src/components/UserDataForm/index.jsx
@@ -19,6 +19,16 @@ const UserDataForm = ({ setUser }) => {
                 <input type="text" id="name" name="name" autoComplete="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
             </div>
             {/* End of Name Section */}
+
+            {/* Name Section */}
+            <div className="mb-5">
+                <label htmlFor="gender" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose your gender</label>
+                <select id="gender" name="gender" autoComplete="gender" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+            </div>
+            {/* End of Gender Section */}
             
             {/* Goal Section */}
             <div className="mb-5">
@@ -99,11 +109,12 @@ const UserDataForm = ({ setUser }) => {
                     <option value="2.5">({">"}2.40) Ex.Competitive cyclist</option>
                 </select>
             </div>
-            {/* End of Activity Level Section */}
+            {/* End of Activity Level Section  */}
             
             <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Calculate</button>
         </form>
     );
 };
+
 
 export default UserDataForm;
