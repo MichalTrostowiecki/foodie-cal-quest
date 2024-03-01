ab7de34770e0259ca299a4d31b2e6c9acc7cf096
Michal_T <michal.trostowiecki@gmail.com>
2024-03-01 22:25:41 +0000

    Add macronutrients calculations and total calories for the user

--- foodie-cal-quest/src/components/CaloriesCalPage/index.jsx
+++ foodie-cal-quest/src/components/CaloriesCalPage/index.jsx
@@ -4,33 +4,84 @@ import UserDataForm from "../UserDataForm"
 const CaloriesCalPage = () => {
 
     const [user, setUser] = useState({});
-    const [userCalories, setUserCalories] = useState(null);
+    const [userCalories, setUserCalories] = useState([]);
 
 
-    const calculateCalories = () => {
+    // Calculate calories based on user activity level
+    const calculateCalories = (userData) => {
         console.log(user);
+        let totalCal = 0;
 
-        //If user is a male use this formula
-        if(user.gender === "male") {
-            const baseCal = 66.47 + (13.75 * user.weightKG) + (5 * user.heightInCM) - (6.75 * user.age);
-            const totalCal = baseCal * user.activityLevel;
-            setUserCalories(Math.round(totalCal));
-
+        // If user is a male use this formula
+        if(userData.gender === "male") {
+            // Base calories calculated
+            const baseCal = 66.47 + (13.75 * userData.weightKG) + (5 * userData.heightInCM) - (6.75 * userData.age);
+            // Base calories increased by the activityLevel of a user
+            totalCal = baseCal * userData.activityLevel;
         } else {
-            const baseCal = 665.1 + (9.53 * user.weightKG) + (1.85 * user.heightInCM) - (4.66 * user.age);
-            const totalCal = baseCal * user.activityLevel;
-            setUserCalories(Math.round(totalCal));
+            const baseCal = 665.1 + (9.53 * userData.weightKG) + (1.85 * userData.heightInCM) - (4.66 * userData.age);
+            totalCal = baseCal * userData.activityLevel;
         }
 
+        return Math.round(totalCal);
     }
+
+    // TotalCal is passed from handleSubmit in UserDataForm Component
+    const calculateMacro = (totalCal, userData) => {
+        const activityLevel = parseFloat(userData.activityLevel);
+        let protein = 0;
+    
+        if (activityLevel <= 1.8) {
+            // Calculate Protein depending on activity level 0.9g/kg for <=1.8 and 1.4g/kg for >1.8
+            protein = Math.round(0.9 * userData.weightKG);
+        } else if (activityLevel > 1.8) {
+            protein = Math.round(1.4 * userData.weightKG);
+        }
+        
+        const proteinCalories = Math.round(protein * 4);
+    
+        // Calculate Carbs
+        const carbsCalories = Math.round((totalCal - proteinCalories) * 0.55);
+        const carbs = Math.round(carbsCalories / 4);
+    
+        // Calculate Fats
+        const fatsCalories = Math.round(totalCal - (proteinCalories + carbsCalories));
+        const fats = Math.round(fatsCalories / 9);
+    
+        setUserCalories({
+            baseCalories: totalCal,
+            protein: { grams: protein, calories: proteinCalories },
+            carbs: { grams: carbs, calories: carbsCalories },
+            fats: { grams: fats, calories: fatsCalories },
+        });
+    };
+    
+
+
 
 
 
     return (
         <div>
-            <UserDataForm setUser={setUser} calculateCalories={calculateCalories}/>
-            <pre>{JSON.stringify(user, null, 2)}</pre>
-            <div>User Calories: {userCalories} kcal</div>
+            <UserDataForm 
+                setUser={setUser}
+                calculateCalories={calculateCalories}
+                setUserCalories={setUserCalories}
+                calculateMacro={calculateMacro}                
+            />
+            {/* Loop through the userCalories just to print the data to the screen */}
+            <div>
+                {Object.entries(userCalories).map(([key, value]) => {
+                    if (typeof value === 'object') {
+                        return (
+                            <div key={key}>
+                                {key.charAt(0).toUpperCase() + key.slice(1)}: {value.grams} g ({value.calories} kcal)
+                            </div>
+                        );
+                    }
+                    return <div key={key}>{key.charAt(0).toUpperCase() + key.slice(1)}: {value}</div>;
+                })}
+            </div>
         </div>
     )
 }
